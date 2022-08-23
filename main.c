8244043d71650a4123d51fe128bc90c91fce1253
ytasan <yasintasan@hotmail.com>
2022-08-23 15:24:02 +0300

    seperate to digits

--- main.c
+++ main.c
@@ -1,10 +1,28 @@
 bool isPalindrome(int x){
-    int DigitCount = 0;   
-    while( x! = 0) {  
-        x = x / 10;  
-        DigitCount++;  
+    int digitCount = 0;   
+    int tempX = x;
+    unsigned int i = 0;
+    unsigned int* digitArr = (unsigned int*) malloc(digitCount * sizeof(unsigned int));
+    
+    while( tempX != 0) {  
+        tempX = tempX / 10;  
+        digitCount++;  
     }  
-    printf("digit number = %d\n", DigitCount);
+    printf("digit number = %d\n", digitCount);
+    printf("number = %d\n", x);
+    
+    tempX = x;    
+    while(tempX > 0) 
+    {
+        int digit = tempX % 10;          
+        digitArr[i] = digit; 
+        i++;
+        tempX = tempX / 10;    
+    }
+    
+    for (i = 0; i < digitCount; i++) {
+        printf("digit = %d\n", digitArr[i]); 
+    }
 
     return false;
 }
