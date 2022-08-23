93a9e0107861d1768f26018be70ea4296e97e5b8
ytasan <yasintasan@hotmail.com>
2022-08-23 14:57:26 +0300

    digit count

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
-
-
 bool isPalindrome(int x){
+    int DigitCount = 0;   
+    while( x! = 0) {  
+        x = x / 10;  
+        DigitCount++;  
+    }  
+    printf("digit number = %d\n", DigitCount);
 
+    return false;
 }
